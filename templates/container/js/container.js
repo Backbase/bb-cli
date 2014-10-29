10793c2684133d1fb5a2ea7cc700730b1d9be030
Guilherme Medeiros <eu@guilhermemedeiros.com.br>
2014-10-29 11:06:51 +0100

    added containers template and scaffolding

---
+++ templates/container/js/container.js
@@ -0,0 +1,23 @@
+/* global b$ */
+(function () {
+    "use strict";
+
+    var Container = b$.bdom
+                    .getNamespace('http://backbase.com/2013/portalView')
+                    .getClass('container');
+ 
+    Container.extend(function (bdomDocument, node) {
+        Container.apply(this, arguments);
+        this.isPossibleDragTarget = true;
+    }, {
+        localName: '<%=template_name%>',
+        // the same as the viewNamespace property in the import XML for the container and in the soy template
+        namespaceURI: '<%=bundle_name%>'
+    }, {
+        template: function(json) {
+            var data = {item: json.model.originalItem};
+            var sTemplate = <%=bundle_name%>.<%=template_name%>(data);
+            return sTemplate;
+        }
+    });
+})();
