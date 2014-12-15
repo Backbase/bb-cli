a073eb46c2fa884c30fe1d87fcd914da6ce9ae02
Guilherme Medeiros <eu@guilhermemedeiros.com.br>
2014-12-15 11:24:57 +0100

    fixing angularjs module on widget template

--- templates/widget/template/js/{widget_name}.js
+++ templates/widget/template/js/{widget_name}.js
@@ -1,18 +1,14 @@
 define(['angular'], function(angular){
 	"use strict";
 
-	angular
-		.module('<%=widget_name%>', [])
-		.controller('main', ['$scope', 'Widget', function($scope, Widget) {
+	angular.module('<%=widget_name%>', [])
+		.controller('main', ['$scope', 'widget', function($scope, widget) {
 			$scope.data = 'Hello from Angular';
 			console.log(widget);
 		}]);
 
 	return function(widget) {
-		angular
-			.module('<%=widget_name%>')
-			.value('Widget', widget);
-
+		angular.module('<%=widget_name%>').value('widget', widget);
 		angular.bootstrap(widget.body, ['<%=widget_name%>'])
 	}
 })
