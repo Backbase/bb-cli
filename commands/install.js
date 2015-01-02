ba13b9fea64fd42666b7ee74d64defc743423266
Robert Haritonov <r@rhr.me>
2015-01-02 17:07:42 +0100

    Improving `bb install` with support to path and web url configuration. `bb i` shorthand Cleaning temp code

--- commands/install.js
+++ commands/install.js
@@ -4,186 +4,73 @@ var chalk = require('chalk');
 var spawn = require('cross-spawn');
 var bower = require('bower');
 var path = require('path');
-var _ = require('lodash');
+var __ = require('lodash-contrib');
+var GenerateRequireConf = require('../lib/generateRequireConf');
 var Q = require('q');
 var fs = require('node-fs-extra');
-var buildConfig = require('bower-requirejs/lib/build-config');
-var JSZip = require("jszip");
-var acorn = require('acorn');
-var escodegen = require('escodegen');
 
 var baseUrl = process.cwd();
 
-// TODO: add flag options
-var options = {
-    requireConfPath: path.join(baseUrl, 'portalserver/src/main/webapp/static/launchpad/conf', 'require-bower-config.js'),
-    bowerBuildConfig: {
-        //exclude: ['underscore', 'jquery'],
-        transitive: true
-    }
-};
-
-var mergeRequireConf = function(input, pathsConfObj) {
-    var mergedOutput = _.clone(input);
-
-    for (var key in mergedOutput.paths) {
-        if (pathsConfObj[key]) {
-            // TODO: Ask user what how to resolve conflict
-            // Delete conflicting dep
-            delete mergedOutput.paths[key];
+// TODO: override options from flags: requireConfPath, bowerBuildConfig
+// TODO: set options from flags - webURL, requirejsConfigs
+var prepareOptions = function(confs){
+    var options = {
+        bowerJSON: confs.bowerJSON,
+        bowerOpts: confs.bowerOpts,
+        requireConfPath: path.join(baseUrl, 'bower_components', 'require-bower-config.js'),
+        bowerBuildConfig: {
+            //exclude: ['underscore', 'jquery'],
+            transitive: true
         }
-    }
-
-    return mergedOutput;
-};
-
-var getPathKeyFromRequireConf = function(astProperties){
-    var output = {};
-
-    astProperties.forEach(function(item){
-        if (item.key.name === 'paths') {
-            output = item.value;
-        }
-    });
-
-    return output;
-};
-
-var parseRequireConf = function(raw){
-    try {
-        var ast = acorn.parse(raw);
-        var output = escodegen.generate(getPathKeyFromRequireConf(ast.body[0].expression.arguments[0].properties), {
-            format: {
-                json: true
-            }
-        });
-        return eval('('+output+')');
-    } catch(e) {
-        console.log('Failed to parse existing Require conf', e);
-        return {};
-    }
-};
-
-var readRequireConf = function(dir){
-    var deferred = Q.defer();
-    var warReg = /.war/g;
-    var haveWar = warReg.test(dir);
-
-    if (haveWar) {
-        var splitDir = dir.split('.war');
-        var pathToWar = splitDir[0] + '.war';
-        var pathToInWar = splitDir[1].substring(1);
-
-        fs.readFile(path.join(baseUrl, pathToWar), function (err, data) {
-            if (err) throw err;
-            var zip = new JSZip(data);
-
-            deferred.resolve(parseRequireConf(zip.file(pathToInWar).asText()));
-        });
-    } else {
-        fs.readFile(dir, 'utf-8', function (err, data) {
-            if (err) throw err;
-
-            deferred.resolve(parseRequireConf(data));
-        });
-    }
-
-    return deferred.promise;
-};
-
-var compareRequireConf = function(inputConf, dependencyGraph) {
-    var deferred = Q.defer();
-    var bowerOptions = dependencyGraph.pkgMeta;
-    var output = _.clone(inputConf);
-    var order = 0;
-    var requireConfList = bowerOptions.requirejsConfigs;
-
-    if (requireConfList) {
-        var read = function(pathToConf){
-            readRequireConf(pathToConf).then(function(pathsConfObj){
-                output = mergeRequireConf(output, pathsConfObj);
-
-                order++;
-
-                if (order < requireConfList.length) {
-                    read(requireConfList[order]);
-                } else {
-                    deferred.resolve(output);
-                }
-            });
-        };
-
-        // Read first file
-        read(requireConfList[order]);
-    } else {
-        deferred.resolve(output);
-    }
-
-    return deferred.promise;
-};
-
-var getComponentPaths = function(allDependencies, bowerComponents) {
-    var output = {
-        paths: {}
     };
 
-    _.forOwn(allDependencies, function (value, key) {
-        // Check if it's node client-side dep
-        if (!bowerComponents.paths.hasOwnProperty(key)) {
-            output.paths[key] = value;
+    if (__.hasPath(confs, 'bowerOpts.directory')) {
+        options.requireConfPath = path.join(baseUrl, confs.bowerOpts.directory, 'require-bower-config.js');
+    }
+
+    return options;
+};
+
+var readBowerConfs = function(){
+    var deferred = Q.defer();
+
+    fs.readJSON('./bower.json', function (err, bowerJSONdata) {
+        if (err) {
+            throw new Error('Could not find bower.json, please init your project first: ', err);
         }
-    });
+        var bowerJSON = bowerJSONdata;
 
-    return output;
-};
+        fs.readJSON('./.bowerrc', function (err, bowerOptsData) {
+            var bowerOpts = bowerOptsData || {};
 
-var writeRequireConf = function(preparedConf){
-    var data = 'require.config(' + JSON.stringify(preparedConf, null, 4) + ');';
-
-    console.log(chalk.green('Generated RequireJS conf:'), data);
-
-    fs.writeFile(options.requireConfPath, data, function (err) {
-      if (err) throw err;
-      console.log(chalk.green('RequireJS conf saved to:'), options.requireConfPath);
-    });
-};
-
-var generateRequireConf = function(){
-    // Getting full dep graph
-    bower.commands.list({})
-        .on('end', function (dependencyGraph) {
-            var bowerPaths = {};
-
-            // Generate cliend-side deps configuration for RequireJS
-            var libsPaths = buildConfig(dependencyGraph, options.bowerBuildConfig);
-
-            // Getting only list of dep paths
-            bower.commands.list({paths: true})
-                .on('end', function (dependencyPaths) {
-                    // Getting list of BB components without main file defined
-                    var componentsPaths = getComponentPaths(dependencyPaths, libsPaths);
-
-                    bowerPaths = _.merge(libsPaths, componentsPaths);
-
-                    // Compare bower paths with existing libs outside bower
-                    compareRequireConf(bowerPaths, dependencyGraph).then(function(mergedConf){
-                        // Proceed to write
-                        writeRequireConf(mergedConf);
-                    });
-                });
+            deferred.resolve({
+                bowerJSON: bowerJSON,
+                bowerOpts: bowerOpts
+            });
         });
+    });
+
+    return deferred.promise;
 };
 
 var install = function(){
-    console.log(chalk.gray('Running Bower install...'));
+    readBowerConfs().then(function(confs){
+        var options = prepareOptions(confs);
 
-    // First, we install all bower deps
-    //spawn('bower', ['install'], {stdio: 'inherit'}).on('close', function () {
-        console.log(chalk.gray('Bower install done, proceed to RequireJS conf generation...'));
+        console.log(chalk.gray('Running Bower install...'));
 
-        // Then we generate RequireJS conf
-        generateRequireConf();
-    //});
+        // First, we install all bower deps
+        spawn('bower', ['install'], {stdio: 'inherit'}).on('close', function () {
+            console.log(chalk.gray('Bower install done, proceed to RequireJS conf generation...'));
+
+            var generate = new GenerateRequireConf(options);
+
+            // Then we generate RequireJS conf
+            generate.getAndWrite();
+        });
+    }).fail(function(err){
+        console.log('Something went wrong, during Bower configuration read: ', err);
+    });
 };
 
 var Install = Command.extend({
