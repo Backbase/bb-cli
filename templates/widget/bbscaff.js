04e941c9a6da0b0208f69f165b6455f4286bf5e2
Guilherme Medeiros <eu@guilhermemedeiros.com.br>
2015-03-11 14:16:53 +0100

    started implementation of bbrest on widget template

--- templates/widget/bbscaff.js
+++ templates/widget/bbscaff.js
@@ -1,3 +1,6 @@
+var path = require('path');
+var config = require(__dirname + '../../../lib/config');
+
 module.exports = function(bbscaff){
     bbscaff.prompt([
         {
@@ -29,17 +32,14 @@ module.exports = function(bbscaff){
                 message: 'Do you want to add this widget to your Enterprise Catalog?'
             }, function(answer){
                 if(answer.add_widget){
+                    config.getCommon().then(function(obj){
+                        obj.bbrest.catalog().post(path.join(process.cwd(), answers.widget_name, 'catalog-'+answers.widget_name+'.xml').then(function(){
+                            bbscaff.logSuccess(answers.widget_name, 'successfully added to your portal enterprise catalog.');
+                        }, function(err){
+                            bbscaff.logError(err);
+                        }));
+                    });
 
-                    //Todo: Implement add widget using bbrest
-                    bbscaff.logError('Not implemented');
-
-                    // addWidget(answers.widget_name, function(err){
-                    //     if(err) {
-                    //         bbscaff.logError(err)
-                    //     } else {
-                    //         bbscaff.logSuccess(answers.widget_name, 'successfully added to your portal enterprise catalog.')
-                    //     }
-                    // })
                 }
             });
         });
