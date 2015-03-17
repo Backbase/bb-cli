c1dae6c5ecf0fe8dfdd7fe4726e0e37fe771a961
Igor Dimitrijevic <igor@backbase.com>
2015-03-17 14:06:03 +0100

    lint, code formatting

--- commands/property.js
+++ commands/property.js
@@ -1,58 +1,58 @@
 var restUtils = require('../lib/restUtils');
-var Command = require('ronin').Command,
-    Q = require('q'),
-    fs = require('fs'),
-    readFile = Q.denodeify(fs.readFile),
-    readDir = Q.denodeify(fs.readdir),
-    _ = require('lodash'),
-    ask = Q.denodeify(require('asking').ask),
-    config = require('../lib/config'),
-    chalk = require('chalk'),
-    bbrest, jxon, cfg;
+var Command = require('ronin').Command;
+var Q = require('q');
+var fs = require('fs');
+var readFile = Q.denodeify(fs.readFile);
+var readDir = Q.denodeify(fs.readdir);
+var _ = require('lodash');
+var ask = Q.denodeify(require('asking').ask);
+var config = require('../lib/config');
+var chalk = require('chalk');
+var bbrest, jxon, cfg;
 
 module.exports = Command.extend({
-  desc: 'Backbase CLI task automation.',
-  help: function () {
-    var title = chalk.bold;
-    var d = chalk.gray;
-    var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
-    r += '\n\t Automates tasks during development of the components.';
-    r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n\n';
-    r += '      -p,  --prop\t\t\t\t\tSubmits changes of the widget propertie to the portal.\n';
-    r += '      -f,  --file\t\t\t\t\tA file to target.\n';
-    //r += '      -w,  --watch\t\t\t\t\tEnables watching for file change.\n';
-    r += '      -v,  --verbose\t\t\t\t\tPrints detailed output.\n';
-    /*
-    r += '\n  ' + title('Examples') + ':\n\n';
-    r += '      bb rest\t\t\t\t\t\tReturns portals defined on the server.\n';
-    r += '      bb rest -t cache -T all -m delete\t\tDeletes all cache.\n';
-    */
-    r += '\n';
-    return r;
-  },
+    desc: 'Backbase CLI task automation.',
+    help: function () {
+        var title = chalk.bold;
+        var d = chalk.gray;
+        var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
+        r += '\n\t Automates tasks during development of the components.';
+        r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n\n';
+        r += '      -p,  --prop\t\t\t\t\tSubmits changes of the widget propertie to the portal.\n';
+        r += '      -f,  --file\t\t\t\t\tA file to target.\n';
+        //r += '      -w,  --watch\t\t\t\t\tEnables watching for file change.\n';
+        r += '      -v,  --verbose\t\t\t\t\tPrints detailed output.\n';
+        /*
+        r += '\n  ' + title('Examples') + ':\n\n';
+        r += '      bb rest\t\t\t\t\t\tReturns portals defined on the server.\n';
+        r += '      bb rest -t cache -T all -m delete\t\tDeletes all cache.\n';
+        */
+        r += '\n';
+        return r;
+    },
 
-  options: {
-    prop: {type: 'boolean', alias: 'p'},
-    file: {type: 'string', alias: 'f'},
-    watch: {type: 'boolean', alias: 'w'},
-    verbose: {type: 'boolean', alias: 'v'}
-  },
+    options: {
+        prop: {type: 'boolean', alias: 'p'},
+        file: {type: 'string', alias: 'f'},
+        watch: {type: 'boolean', alias: 'w'},
+        verbose: {type: 'boolean', alias: 'v'}
+    },
 
-  run: function () {
-    return config.getCommon(this.options)
-    .then(function(r) {
-        config = r.config;
-        if (config.cli.prop) {
-            return readDir(process.cwd())
-            .then(function(files) {
-                return init(r, files);
-            });
-        }
-    })
-    .fail(function(e) {
-        console.log(chalk.red('bb property'), e.toString());
-    });
-  }
+    run: function () {
+        return config.getCommon(this.options)
+        .then(function(r) {
+            config = r.config;
+            if (config.cli.prop) {
+                return readDir(process.cwd())
+                .then(function(files) {
+                    return init(r, files);
+                });
+            }
+        })
+        .fail(function(e) {
+            console.log(chalk.red('bb property'), e.toString());
+        });
+    }
 });
 
 function init(r, files) {
@@ -65,7 +65,7 @@ function init(r, files) {
     return getLocal(xmlFileName)
     .then(function(local) {
         var keys = _.keys(local.catalog);
-        if (keys.length !== 1) console.log(chalk.orange('Warning'), 'Only one component per catalog is allowed. Found:', keys);
+        if (keys.length !== 1) console.log(chalk.yellow('Warning'), 'Only one component per catalog is allowed. Found:', keys);
         cType = keys[0];
         cName = local.catalog[keys[0]].name;
         lps = local.catalog[keys[0]].properties.property;
@@ -82,18 +82,18 @@ function init(r, files) {
             ops.sort(sortByName);
 
             compare(lps, ops);
-            
+
             return bbrest.catalog().put(local)
             .then(function(r) {
                 restUtils.onResponse(r, bbrest, cfg.cli);
             });
-            
+
         });
     });
 }
 
 function findXmlFile(files) {
-    var f;
+    var f, i;
     for (i = 0; i < files.length; i++) {
         if (files[i].substr(files[i].lastIndexOf('.')) === '.xml') {
             f = files[i];
@@ -101,7 +101,7 @@ function findXmlFile(files) {
         }
     }
     if (!f) {
-        doError('xml file is not found in the current directory');
+        console.log(chalk.red('bb property'), 'xml file is not found in the current directory');
     }
     return f;
 }
@@ -111,7 +111,7 @@ function getLocal(fName) {
         try {
             return jxon.stringToJs(s.toString());
         } catch(e) {
-            doError('problem parsing', fName);
+            console.log(chalk.red('bb property'), 'problem parsing xml file');
         }
     });
 }
@@ -150,6 +150,7 @@ function compare(lps, ops) {
         lps.push(ov);
     }
 }
+/*
 function addPropDefaults(v) {
     var o = _.assign({
         $readonly: false,
@@ -157,6 +158,7 @@ function addPropDefaults(v) {
     }, v);
     _.assign(v, o);
 }
+*/
 function sortByName(a, b) {
     if (a.$name > b.$name) return 1;
     else if (a.$name < b.$name) return -1;
