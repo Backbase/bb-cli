ed6c87e6c5497d0e108a5c2aeac379fbdcc14d00
Robert Haritonov <r@rhr.me>
2015-04-12 15:57:23 +0200

    Fixing lint and CS errors, optimizing code, error handling, help section.

--- commands/export.js
+++ commands/export.js
@@ -1,54 +1,100 @@
 var inquirer = require("inquirer");
 var chalk = require('chalk');
-var _ = require('lodash');
 var util = require('../lib/util');
 var bbmodel = require('../lib/bbmodel');
-
 var clui = require('clui');
 var loading = new clui.Spinner('Please wait...');
 
 var Command = require('ronin').Command;
 
+var normalizeContextName = function(contextName){
+    return contextName === '/' ? '' : contextName;
+};
 
 module.exports = Command.extend({
-	desc: 'export portal model into xml files. This files can be imported trough Yapi.',
+    help: function () {
+        var title = chalk.bold;
+        var d = chalk.gray;
+        var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
+        r += '\n\t Export portal model into xml files. This files can be then imported trough Yapi.';
+        r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n\n';
+        r += '      -p,  --portal <string> \t' + d('prompt') + ' \t\t Pre-define portal name.\n';
+        r += '      -c,  --context <string> \t' + d('portalserver') + '\t Pre-define context name.\n';
+        return r;
+    },
 
-	run: function (portal_name) {
+    options: {
+        portal: {
+            type: 'string',
+            alias: 'p'
+        },
+        context: {
+            type: 'string',
+            alias: 'c'
+        }
+    },
 
-		if(!portal_name) {
-			bbmodel.listPortals(function(portals){
-				inquirer.prompt([{message: 'Choose the portal you want to export', name: 'portal_name', type: 'list', choices: portals}], function(answers){
-					loading.start()
-					bbmodel.getPortalModel(answers.portal_name, function(xml){
-						loading.stop()
-						areYouSure(function(){
-							bbmodel.exportPortal(xml, answers.portal_name, process.cwd(), function(file){
-								util.ok(file)
-							})
-						})
-					})
-				})
-			})
-		} else {
-			loading.start()
-			bbmodel.getPortalModel(portal_name, function(xml){
-				loading.stop()
-				if(xml) {
-					areYouSure(function(){
-						bbmodel.exportPortal(xml, portal_name, process.cwd(), function(file){
-							util.ok(file)
-						})
-					})
-				} else {
-					util.err("Portal doesn't exist or cannot be exported.")
-				}
-			})
-		}
+    run: function (portalName, contextName) {
+        contextName = normalizeContextName(contextName);
 
-		function areYouSure(cb){
-			inquirer.prompt([{message: 'You are about to export your portal model into ' + chalk.inverse(process.cwd()) + '. ' + chalk.red('This may override your files. Are you sure?'), name: 'confirm', type: 'confirm'}], function(answers){
-				if(answers.confirm) cb()
-			})
-		}
-	}
+        inquirer.prompt([{
+            message: 'Server context name',
+            name: 'contextName',
+            type: 'input',
+            default: 'portalserver',
+            filter: function(answer){
+                return normalizeContextName(answer);
+            },
+            when: function() {
+                // Ask only of param was not provided
+                return typeof contextName !== 'string';
+            }
+        }], function (answersLevelOne) {
+            var context = typeof answersLevelOne.contextName === 'string' ? answersLevelOne.contextName : contextName;
+
+            // Request portals list, if portal is not provided
+            if (!portalName) {
+                bbmodel.listPortals(context, function (portals) {
+                    if (!portals) return util.err('Could not get portal list.');
+
+                    inquirer.prompt([{
+                        message: 'Choose the portal you want to export',
+                        name: 'portalName',
+                        type: 'list',
+                        choices: portals
+                    }], function (answersLevelTwo) {
+                        exportPortal(answersLevelTwo.portalName, context);
+                    });
+                });
+            } else {
+                exportPortal(portalName, context);
+            }
+        });
+
+        function exportPortal(portalName, contextName) {
+            loading.start();
+            bbmodel.getPortalModel(portalName, contextName, function (xml) {
+                loading.stop();
+                if (xml) {
+                    areYouSure(function () {
+                        bbmodel.exportPortal(xml, portalName, process.cwd(), function (file) {
+                            util.ok(file);
+                        });
+                    });
+                } else {
+                    util.err("Portal doesn't exist or cannot be exported.");
+                }
+            });
+        }
+
+        function areYouSure(cb) {
+            inquirer.prompt([{
+                message: 'You are about to export your portal model into ' + chalk.green(process.cwd()) + '. ' + chalk.red('\n  This may override your files. Are you sure?'),
+                name: 'confirm',
+                type: 'confirm'
+            }], function (answers) {
+                if (answers.confirm) cb();
+            });
+        }
+    }
 });
