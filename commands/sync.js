fc1900f63769ef0e8533b6ddb8e6703eee035885
Robert Haritonov <r@rhr.me>
2015-04-13 16:52:31 +0200

    Renaming `prop` to `sync` + some fixes

--- commands/sync.js
+++ commands/sync.js
@@ -5,52 +5,42 @@ var fs = require('fs');
 var readFile = Q.denodeify(fs.readFile);
 var readDir = Q.denodeify(fs.readdir);
 var _ = require('lodash');
-var ask = Q.denodeify(require('asking').ask);
+var inquirer = require('inquirer');
 var config = require('../lib/config');
 var chalk = require('chalk');
 var bbrest, jxon, cfg;
 
 module.exports = Command.extend({
-    desc: 'Backbase CLI task automation.',
     help: function () {
         var title = chalk.bold;
         var d = chalk.gray;
         var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
-        r += '\n\t Automates tasks during development of the components.';
+        r += '\n\t Syncs local XML model with remote.';
         r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n\n';
-        r += '      -p,  --prop\t\t\t\t\tSubmits changes of the widget propertie to the portal.\n';
-        r += '      -f,  --file\t\t\t\t\tA file to target.\n';
-        //r += '      -w,  --watch\t\t\t\t\tEnables watching for file change.\n';
-        r += '      -v,  --verbose\t\t\t\t\tPrints detailed output.\n';
-        /*
-        r += '\n  ' + title('Examples') + ':\n\n';
-        r += '      bb rest\t\t\t\t\t\tReturns portals defined on the server.\n';
-        r += '      bb rest -t cache -T all -m delete\t\tDeletes all cache.\n';
-        */
-        r += '\n';
+        r += '      -f,  --file <string>\t' + d('first xml file') + '\t\t A file to target.\n';
+        r += '      -c,  --context <string>\t' + d('portalserver') + '\t\t Portal server context (for other options use `.bbrc`).\n';
+        //r += '      -w,  --watch <boolean>\t' + d('false') + '\t\t\t Enables watching for file change.\n';
+        r += '      -v,  --verbose\t\t' + d('false') + '\t\t\t Prints detailed output.\n';
         return r;
     },
 
     options: {
-        prop: {type: 'boolean', alias: 'p'},
         file: {type: 'string', alias: 'f'},
-        watch: {type: 'boolean', alias: 'w'},
+        context: {type: 'context', alias: 'c'},
+        //watch: {type: 'boolean', alias: 'w'},
         verbose: {type: 'boolean', alias: 'v'}
     },
 
     run: function () {
         return config.getCommon(this.options)
         .then(function(r) {
-            config = r.config;
-            if (config.cli.prop) {
-                return readDir(process.cwd())
+            return readDir(process.cwd())
                 .then(function(files) {
                     return init(r, files);
                 });
-            }
         })
         .fail(function(e) {
-            console.log(chalk.red('bb property'), e.toString());
+            console.log(chalk.red('bb prop error: '), e);
         });
     }
 });
@@ -61,6 +51,8 @@ function init(r, files) {
     cfg = r.config;
     var xmlFileName = cfg.cli.file || findXmlFile(files);
     var cType, cName, lps, ops;
+
+    if (cfg.cli.context) bbrest.config.context = cfg.cli.context;
 
     return getLocal(xmlFileName)
     .then(function(local) {
@@ -87,7 +79,6 @@ function init(r, files) {
             .then(function(r) {
                 restUtils.onResponse(r, bbrest, cfg.cli);
             });
-
         });
     });
 }
@@ -101,44 +92,54 @@ function findXmlFile(files) {
         }
     }
     if (!f) {
-        console.log(chalk.red('bb property'), 'xml file is not found in the current directory');
+        console.log(chalk.red('bb prop error:'), 'xml file is not found in the current directory');
     }
     return f;
 }
+
 function getLocal(fName) {
     return readFile(fName)
     .then(function(s) {
         try {
             return jxon.stringToJs(s.toString());
         } catch(e) {
-            console.log(chalk.red('bb property'), 'problem parsing xml file');
+            console.log(chalk.red('bb prop error:'), 'problem parsing xml file');
         }
     });
 }
+
 function getOrigin(cName, local) {
     return bbrest.catalog(cName).get()
-    .then(function(r) {
-        if (r.statusCode === 404 || r.error) {
-            return ask("'" + cName + "' is not defined on the server. Submit model?", {default: 'Y'})
-            .then(function(r) {
-                if (r === 'Y') {
-                    return bbrest.catalog().post(local)
-                    .then(function(r) {
-                        if (r.statusCode === 204) {
-                            console.log(chalk.green('Done.'));
-                            return false;
-                        } else restUtils.onResponse(r, bbrest, cfg);
-                    });
-                } else {
-                    return false;
-                }
-            });
-        } else {
-            return jxon.stringToJs(_.unescape(r.body));
-        }
-    });
+        .then(function(r) {
+            if (r.statusCode === 404 || r.error) {
+                var deferred = Q.defer();
 
+                inquirer.prompt([{
+                    message: "'" + cName + "' is not defined on the server. Submit model?",
+                    name: 'submitModel',
+                    type: 'confirm'
+                }], function(answers){
+                    if (answers.submitModel) {
+                        bbrest.catalog().post(local)
+                            .then(function (r) {
+                                if (r.statusCode === 204) {
+                                    console.log(chalk.green('Submitted.'));
+                                } else restUtils.onResponse(r, bbrest, cfg.cli);
+
+                                deferred.resolve(false);
+                            });
+                    } else {
+                        deferred.resolve(false);
+                    }
+                });
+
+                return deferred.promise;
+            } else {
+                return jxon.stringToJs(_.unescape(r.body));
+            }
+        });
 }
+
 function compare(lps, ops) {
     var lKeys = _.pluck(lps, '$name');
     var oKeys = _.pluck(ops, '$name');
@@ -146,10 +147,15 @@ function compare(lps, ops) {
 
     for (var ov, i = 0; i < toDelete.length; i++) {
         ov = _.find(ops, {$name: toDelete[i]});
+
+        // Skip g:preferences fields, since they are not in xml
+        if (ov.$viewHint.indexOf("select-one") > -1) return;
+
         ov.$markedForDeletion = true;
         lps.push(ov);
     }
 }
+
 /*
 function addPropDefaults(v) {
     var o = _.assign({
@@ -159,11 +165,13 @@ function addPropDefaults(v) {
     _.assign(v, o);
 }
 */
+
 function sortByName(a, b) {
     if (a.$name > b.$name) return 1;
     else if (a.$name < b.$name) return -1;
     else return 0;
 }
+
 function removeEmpty(v) {
     return v.value._ !== undefined;
 }
