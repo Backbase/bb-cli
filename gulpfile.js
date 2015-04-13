149be325b074c1e4fe78e2418153197a56a334a2
Robert Haritonov <r@rhr.me>
2015-04-13 08:50:42 +0200

    Adding mocha to default grunt task, cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jshintStylish = require('jshint-stylish');
 var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
 
 var src = [
     'bin/*',
     'commands/**/*.js',
-    'lib/**/*.js'
+    'lib/**/*.js',
+    // Till the merge of PR #26
+    '!commands/export.js',
+    '!lib/bbmodel.js'
 ];
+
+gulp.task('test', function () {
+    return gulp.src('test/**/*.js', {read: false})
+        .pipe(mocha());
+});
 
 gulp.task('jshint', function(){
     return gulp.src(src)
@@ -20,4 +29,4 @@ gulp.task('jscs', function(){
         .pipe(jscs());
 });
 
-gulp.task('default', ['jshint', 'jscs']);
+gulp.task('default', ['test', 'jshint', 'jscs']);
