9ade873bd6eef2edc7fef2464056f3578c764b88
Igor Dimitrijevic <igor@backbase.com>
2015-05-26 12:15:16 +0200

    add model.xml save to sync command

--- commands/sync.js
+++ commands/sync.js
@@ -3,11 +3,13 @@ var Command = require('ronin').Command;
 var Q = require('q');
 var fs = require('fs');
 var readFile = Q.denodeify(fs.readFile);
+var writeFile = Q.denodeify(fs.writeFile);
 var readDir = Q.denodeify(fs.readdir);
 var _ = require('lodash');
 var inquirer = require('inquirer');
 var config = require('../lib/config');
 var chalk = require('chalk');
+var formattor = require('formattor');
 var bbrest, jxon, cfg;
 
 module.exports = Command.extend({
@@ -19,6 +21,7 @@ module.exports = Command.extend({
         r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n\n';
         r += '      -f,  --file <string>\t' + d('first xml file') + '\t\t A file to target.\n';
         r += '      -c,  --context <string>\t' + d('portalserver') + '\t\t Portal server context (for other options use `.bbrc`).\n';
+        r += '      -s,  --save <string>\t' + d('') + '\t\t\t Name of the server item which model should be exported to a file.\n';
         //r += '      -w,  --watch <boolean>\t' + d('false') + '\t\t\t Enables watching for file change.\n';
         r += '      -v,  --verbose\t\t' + d('false') + '\t\t\t Prints detailed output.\n';
         return r;
@@ -27,6 +30,7 @@ module.exports = Command.extend({
     options: {
         file: {type: 'string', alias: 'f'},
         context: {type: 'context', alias: 'c'},
+        save: {type: 'string', alias: 's'},
         //watch: {type: 'boolean', alias: 'w'},
         verbose: {type: 'boolean', alias: 'v'}
     },
@@ -45,42 +49,45 @@ module.exports = Command.extend({
     }
 });
 
+
+var propsToKeep = {
+    name: true,
+    contextItemName: true,
+    extendedItemName: true,
+    properties: true
+};
 function init(r, files) {
     bbrest = r.bbrest;
     jxon = r.jxon;
     cfg = r.config;
     var xmlFileName = cfg.cli.file || findXmlFile(files);
-    var cType, cName, lps, ops;
 
     if (cfg.cli.context) bbrest.config.context = cfg.cli.context;
 
-    return getLocal(xmlFileName)
-    .then(function(local) {
-        var keys = _.keys(local.catalog);
-        if (keys.length !== 1) console.log(chalk.yellow('Warning'), 'Only one component per catalog is allowed. Found:', keys);
-        cType = keys[0];
-        cName = local.catalog[keys[0]].name;
-        lps = local.catalog[keys[0]].properties.property;
-
-        lps.sort(sortByName);
-
-        return getOrigin(cName, local)
-        .then(function(origin) {
-            // if item is not defined, r is false
-            if (origin === false) return true;
-            ops = origin.catalog[keys[0]].properties.property;
-
-            ops = _.filter(ops, removeEmpty);
-            ops.sort(sortByName);
-
-            compare(lps, ops);
-
-            return bbrest.catalog().put(local)
-            .then(function(r) {
-                restUtils.onResponse(r, bbrest, cfg.cli);
+    // if there is no xml file, get model from server and save it
+    if (files.indexOf(xmlFileName) === -1) {
+        // if the name of the item to be saved is defined...
+        if (cfg.cli.save) return saveFile(cfg.cli.file || 'model.xml', cfg.cli.save);
+        // otherwise read bower
+        else if (files.indexOf('bower.json') !== -1) {
+            return readFile('bower.json')
+            .then(function(result) {
+                return saveFile(cfg.cli.file || 'model.xml', JSON.parse(result).name);
+            })
+            .fail(function() {
+                console.log('Insufficient data to write model xml file. bower.json read failed');
             });
-        });
-    });
+        } else {
+            var prom = Q.defer();
+            prom.reject();
+            console.log('Insufficient data to write model xml file.');
+            return prom;
+        }
+    // otherwise submit existing file
+    } else {
+        return submitFile(xmlFileName);
+    }
+
 }
 
 function findXmlFile(files) {
@@ -91,10 +98,54 @@ function findXmlFile(files) {
             break;
         }
     }
-    if (!f) {
-        console.log(chalk.red('bb prop error:'), 'xml file is not found in the current directory');
-    }
     return f;
+}
+
+function saveFile(fname, itemName) {
+    var defer = Q.defer();
+    inquirer.prompt([{
+        message: "'" + fname + "' does not exist. Create one?",
+        name: 'saveModel',
+        type: 'confirm'
+    }], function(answers) {
+        if (answers.saveModel) {
+            defer.resolve(writeModelFile(fname, itemName));
+        } else {
+            defer.reject('Saving aborted.');
+        }
+    });
+    return defer.promise;
+}
+
+function submitFile(xmlFileName) {
+    var itemType, itemName, localProps, serverProps;
+    return getLocal(xmlFileName)
+    .then(function(local) {
+        var keys = _.keys(local.catalog);
+        if (keys.length !== 1) console.log(chalk.yellow('Warning'), 'Only one component per catalog is allowed. Found:', keys);
+        itemType = keys[0];
+        itemName = local.catalog[itemType].name;
+        localProps = local.catalog[itemType].properties.property;
+
+        localProps.sort(sortByName);
+
+        return getOrigin(itemName, local)
+        .then(function(origin) {
+            // if item is not defined, r is false
+            if (origin === false) return true;
+            serverProps = origin.catalog[keys[0]].properties.property;
+
+            serverProps = _.filter(serverProps, removeEmpty);
+            serverProps.sort(sortByName);
+
+            compare(localProps, serverProps);
+
+            return bbrest.catalog().put(local)
+            .then(function(r) {
+                restUtils.onResponse(r, bbrest, cfg.cli);
+            });
+        });
+    });
 }
 
 function getLocal(fName) {
@@ -108,26 +159,26 @@ function getLocal(fName) {
     });
 }
 
-function getOrigin(cName, local) {
-    return bbrest.catalog(cName).get()
+function getOrigin(itemName, local) {
+    return bbrest.catalog(itemName).get()
         .then(function(r) {
             if (r.statusCode === 404 || r.error) {
                 var deferred = Q.defer();
 
                 inquirer.prompt([{
-                    message: "'" + cName + "' is not defined on the server. Submit model?",
+                    message: "'" + itemName + "' is not defined on the server. Submit model?",
                     name: 'submitModel',
                     type: 'confirm'
                 }], function(answers){
                     if (answers.submitModel) {
                         bbrest.catalog().post(local)
-                            .then(function (r) {
-                                if (r.statusCode === 204) {
-                                    console.log(chalk.green('Submitted.'));
-                                } else restUtils.onResponse(r, bbrest, cfg.cli);
+                        .then(function (r) {
+                            if (r.statusCode === 204) {
+                                console.log(chalk.green('Submitted.'));
+                            } else restUtils.onResponse(r, bbrest, cfg.cli);
 
-                                deferred.resolve(false);
-                            });
+                            deferred.resolve(true);
+                        });
                     } else {
                         deferred.resolve(false);
                     }
@@ -140,20 +191,38 @@ function getOrigin(cName, local) {
         });
 }
 
-function compare(lps, ops) {
-    var lKeys = _.pluck(lps, '$name');
-    var oKeys = _.pluck(ops, '$name');
+function compare(localProps, serverProps) {
+    var lKeys = _.pluck(localProps, '$name');
+    var oKeys = _.pluck(serverProps, '$name');
     var toDelete = _.difference(oKeys, lKeys);
 
     for (var ov, i = 0; i < toDelete.length; i++) {
-        ov = _.find(ops, {$name: toDelete[i]});
+        ov = _.find(serverProps, {$name: toDelete[i]});
 
         // Skip g:preferences fields, since they are not in xml
         if (ov.$viewHint.indexOf("select-one") > -1) return;
 
         ov.$markedForDeletion = true;
-        lps.push(ov);
+        localProps.push(ov);
     }
+}
+
+function writeModelFile(fname, itemName) {
+    return bbrest.catalog(itemName).get()
+    .then(function(res) {
+        var jx = jxon.stringToJs(_.unescape(res.body));
+        delete (jx.catalog.$totalSize);
+        for (var k in jx.catalog.widget) if (!propsToKeep[k]) delete (jx.catalog.widget[k]);
+        jx = '<?xml version="1.0" encoding="UTF-8"?>' + jxon.jsToString(jx);
+        jx = formattor(jx, {method: 'xml'});
+        return writeFile(fname, jx)
+        .then(function() {
+            console.log(fname + ' saved.');
+        });
+    })
+    .catch(function(e) {
+        console.log(e);
+    });
 }
 
 /*
