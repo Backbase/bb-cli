8ca8381f4165929dedbc49ee58d236e95c53c2c9
Stéphane P. Péricat <stephane@backbase.com>
2015-06-08 14:20:33 +0200

    lp12 widget template

---
+++ templates/lp12-widget/bbscaff.js
@@ -0,0 +1,42 @@
+module.exports = function(bbscaff) {
+    bbscaff.prompt([
+        {
+            name: 'widget_title',
+            message: 'Widget title'
+        },
+        {
+            name: 'widget_name',
+            message: 'Widget name',
+            'default': function(answers) {
+                return answers.widget_title.replace(/ /gi, '-').toLowerCase();
+            }
+        },
+        {
+            name: 'widget_author',
+            message: 'Widget author',
+            'default': 'Backbase <practices@backbase.com>'
+        },
+        {
+            name: 'widget_version',
+            message: 'Widget version',
+            'default': '1.0.0'
+        },
+        {
+            name: 'widget_description',
+            message: 'Widget description',
+            'default': 'A fine launchpad 12 widget.'
+        },
+        {
+            name: 'bundle_name',
+            message: 'Bundle name',
+            'default': bbscaff.getCurrentBundle()
+        },
+        {
+            name: 'bundle_prefix',
+            message: 'Bundle prefix',
+            'default': bbscaff.getPrefix(bbscaff.getCurrentBundle())
+        }
+    ], function(answers){
+        bbscaff.generate(answers, answers.widget_name);
+    });
+};
