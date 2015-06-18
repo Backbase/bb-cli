81af7951a9dfaf91745cc3a2bda4bce5cc0455b5
Robert Haritonov <r@rhr.me>
2015-06-18 18:10:37 +0200

    rewamp bb install

--- commands/install.js
+++ commands/install.js
@@ -9,22 +9,23 @@ var restUtils = require('../lib/restUtils');
 var depUtils = require('../lib/depUtils');
 var configLib = require('../lib/config');
 var q = require('q');
+var bower = require('bower');
 
 var baseUrl = process.cwd();
 
 var config = {
     customArgs: [
         '--catalog','-C',
-        '--web-url',
+        '--base-url',
         '--require-confs'
     ]
 };
 
-var applyBBJSONconfChanges = function(bowerRc, argOptions){
-    if (argOptions['web-url']) bowerRc.dependenciesWebUrl = argOptions['web-url'];
-    if (argOptions['require-confs']) bowerRc.requirejsConfigs = argOptions['require-confs'].split(',');
+var fillConfWithArgs = function(bbRc, argOptions){
+    if (argOptions['base-url']) bbRc.requirejsBaseUrl = argOptions['base-url'];
+    if (argOptions['require-confs']) bbRc.requirejsConfigs = argOptions['require-confs'].split(',');
 
-    return bowerRc;
+    return bbRc;
 };
 
 var install = function(componentEndpoint){
@@ -34,25 +35,25 @@ var install = function(componentEndpoint){
     q.all([
         configLib.getBower(),
         configLib.getBowerRc(),
-        configLib.getBb()
-    ]).spread(function (bowerJSON, bowerRc, bbJSON) {
+        configLib.getBbRc()
+    ]).spread(function (bowerJSON, bowerConf, bbRc) {
         var bowerCommand = ['install'];
         var cmdArgs = process.argv;
         var argCatalog = cmdArgs.indexOf('--catalog') > -1 || cmdArgs.indexOf('-C') > -1;
         var msg = 'Bower install done, proceed to RequireJS conf generation...';
 
         // Adding default directory field
-        if (!bowerRc.directory) bowerRc.directory = path.join(baseUrl, 'bower_components');
+        if (!bowerConf.directory) bowerConf.directory = path.join(baseUrl, 'bower_components');
 
         // Applying arguments overrides
-        bbJSON = applyBBJSONconfChanges(bbJSON, that.options);
+        bbRc = fillConfWithArgs(bbRc, that.options);
 
         // If installing component by name
         if (componentEndpoint){
 
             // If we install local component, we need to first delete previous one
             if (componentEndpoint.substring(0, 2) === './' || componentEndpoint[0] === '/') {
-                depUtils.cleanLocalComponent(path.join(baseUrl, bowerRc.directory), componentEndpoint);
+                depUtils.cleanLocalComponent(path.join(baseUrl, bowerConf.directory), componentEndpoint);
             }
 
             msg = 'Component "'+ componentEndpoint +'" install done, proceed to RequireJS conf generation...';
@@ -69,29 +70,52 @@ var install = function(componentEndpoint){
         console.log(chalk.gray('Running Bower install...'));
 
         // First, we install all bower deps
-        spawn('bower', bowerCommand, {stdio: 'inherit'}).on('close', function () {
-            var generateRJSConf = new GenerateRequireConf(baseUrl, bowerJSON, bowerRc, bbJSON);
+        spawn('bower', bowerCommand, {stdio: 'inherit'})
+            .on('close', function () {
+                var generateRJSConf = new GenerateRequireConf(baseUrl, bowerJSON, bowerConf, bbRc);
 
-            console.log(chalk.gray(msg));
+                console.log(chalk.gray(msg));
 
-            // Then we generate RequireJS conf
-            generateRJSConf.process().then(function(confs){
-                // And submit if specified arguments are set
-                var componentPath;
+                // Then we generate RequireJS conf
+                generateRJSConf.process().then(function(output){
+                    var componentPathAbsolute;
+                    var noName;
 
-                if (componentEndpoint) {
-                    var componentName = depUtils.getComponentNameFromMeta(componentEndpoint, confs.pkgMeta);
+                    if (componentEndpoint) {
+                        if (componentEndpoint.split('/').length === 1) {
+                            componentPathAbsolute = path.join(baseUrl, output.fsPaths[componentEndpoint]);
 
-                    componentPath = path.join(baseUrl, confs.customComponents[componentName]);
-                }
+                            console.log('componentPathAbsolute', componentPathAbsolute);
 
-                if (argCatalog) restUtils.submitToPortal(baseUrl, confs.customComponents, false, componentPath);
-            }).fail(function(err){
-                console.log(chalk.red('Something went wrong during requirejs configuration generation: '), err);
+                        } else {
+                            // If we're asked to install relative component, we can't get it's name and path
+                            noName = true;
+                        }
+                    }
+
+                    if (argCatalog) {
+                        if (!noName) {
+                            restUtils.submitToPortal(baseUrl, output.fsPaths, false, componentPathAbsolute);
+                        } else {
+                            // Get component name by endpoint
+                            bower.commands.info(componentEndpoint)
+                                .on('end', function (componentInfo) {
+                                    var componentName = componentInfo.latest.name;
+                                    var componentPath = path.join(baseUrl, output.fsPaths[componentName]);
+
+                                    restUtils.submitToPortal(baseUrl, {}, false, componentPath);
+                                });
+                        }
+                    }
+                }).fail(function(err){
+                    console.log(chalk.red('Something went wrong during requirejs configuration generation: '), err);
+                    console.log(err.stack);
+                });
             });
-        });
+
     }).fail(function(err){
         console.log(chalk.red('Something went wrong, during Bower configuration read: '), err);
+        console.log(err.stack);
     });
 };
 
@@ -101,14 +125,13 @@ var Install = Command.extend({
         var title = chalk.bold;
         var d = chalk.gray;
         var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
-        r += '\n\t bb ' + this.name + ' <endpoint> [<endpoint> ..] [OPTIONS]';
-        r += '\n\n\t Installs all or specified bower dependencies, generates RequireJS configuration and uploads component model to portal.';
-        r += '\n\n\t Some args could be set through bb.json like `"requirejsConfigs":[]` and `"dependenciesWebUrl":""`.';
-        r += '\n\t Also accepts `bower install` arguments like --save, -save-dev, --production, check `bower install -h`';
+        r += '\n\t bb ' + this.name + ' <bower-endpoint> [<bower-endpoint> ..] [OPTIONS]';
+        r += '\n\n\t Installs all or specified Bower dependencies, generates RequireJS configuration and uploads component model to portal on request.';
+        r += '\n\t Also accepts `bower install` arguments like --save, -save-dev, --production, check `bower install -h`.';
         r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n\n';
-        r += '      -C,  --catalog <boolean>\t\t' + d('false') + '\t\t\t\tUpload components to CXP via REST after install.\n';
-        r += '           --web-url <string>\t\t' + d('same/as/bower_components') + '\tWeb path to bower components directory.\n';
-        r += '           --require-confs <string>\t\t\t' + '\t\tComa seperated list of relative paths to existing require configuration.\n';
+        r += '      -C,  --catalog <boolean>\t\t' + d('false') + '\t\t\tUpload components to CXP via REST after install.\n';
+        r += '           --base-url <string>\t\t' + d('bower_components/path') + '\tWeb path to bower components directory.\n';
+        //r += '           --require-confs <string>\t\t\t' + '\t\tComa seperated list of relative paths to existing require configuration.\n';
         r += '\n  ' + title('Examples') + ':\n\n';
         r += '      bb install\t\t\tInstalls all Bower dependencies and runs requirejs conf generation.\n';
         r += '      bb install jquery\t\t\tInstalls jquery component and runs rjs-conf generation.\n';
