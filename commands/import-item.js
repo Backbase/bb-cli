488ccf9b66a5a1a14667faa5240c0ed0b725cda0
Igor Dimitrijevic <igor@backbase.com>
2015-08-05 12:30:53 +0200

    add watch option on import-item

--- commands/import-item.js
+++ commands/import-item.js
@@ -5,10 +5,12 @@ var clui = require('clui');
 var _ = require('lodash');
 var loading = new clui.Spinner('Please wait...');
 var jxon = require('jxon');
+var watch = require('watch');
 
 var zipDir = require('../lib/zipDir');
 
 var Command = require('ronin').Command;
+var exclude = ['.git', '.gitignore', 'bower_components', 'node_modules'];
 
 var bbrest, jxon, cfg;
 
@@ -19,7 +21,8 @@ module.exports = Command.extend({
         var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
         r += '\n\t Zips and imports item.';
         r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description\n';
-        r += '      -t,  --target <string>\t\t' + '\t\tDir to import.\n\n';
+        r += '      -t,  --target <string>\t\t' + '\t\tDir to import.\n';
+        r += '      -w,  --watch <boolean>\t\t' + '\t\tWatch for file changes in the current dir and autosubmit.\n\n';
 
         r += '      -H,  --host <string>\t\t' + d('localhost') + '\tThe host name of the server running portal foundation.\n';
         r += '      -P,  --port <number>\t\t' + d('7777') + '\t\tThe port of the server running portal foundation.\n';
@@ -31,7 +34,8 @@ module.exports = Command.extend({
     },
 
     options: {
-        target: {type: 'string', alias: 't', default: './'}
+        target: {type: 'string', alias: 't', default: './'},
+        watch: {type: 'boolean', alias: 'w'}
     },
 
     run: function () {
@@ -43,25 +47,60 @@ module.exports = Command.extend({
             jxon = r.jxon;
             cfg = r.config.cli;
 
-            return zipDir(cfg.target, exclude)
-            .then(function(zip) {
-                return bbrest.importItem().file(zip.path).post()
-                .then(function(r) {
-                    var body = jxon.stringToJs(_.unescape(r.body)).import;
-                    if (body.level === 'ERROR') throw new Error(body.message);
-                    zip.clean();
-                    ok(r);
-                })
-                .catch(function(err) {
-                    error(err);
-                });
-            });
+            if (cfg.watch) {
+                watch.watchTree(cfg.target, {
+                    ignoreDotFiles: true,
+                    ignoreUnreadableDir: true,
+                    ignoreNotPermitted: true,
+                    filter: function(fileName) {
+                        var v = exclude.indexOf(fileName);
+                        return (v === -1);
+                    }
+                }, onWatch);
+                console.log(chalk.cyan('Watching...'));
+            }
+
+            return run();
         });
 
     }
 });
 
-var exclude = ['.git', '.gitignore', 'bower_components', 'node_modules'];
+function run() {
+    return zipDir(cfg.target, exclude)
+    .then(function(zip) {
+        return bbrest.importItem().file(zip.path).post()
+        .then(function(r) {
+            var body = jxon.stringToJs(_.unescape(r.body)).import;
+            if (body.level === 'ERROR') throw new Error(body.message);
+            zip.clean();
+            ok(r);
+        })
+        .catch(function(err) {
+            error(err);
+        });
+    });
+}
+
+function onWatch(fileName, curStat, prevStat) {
+    // if (typeof f === 'object' && prevStat === null && curStat === null) {
+    //     // Finished walking the tree
+    //     // file is object where key is fileName and value is stat
+    // } else
+    if (prevStat === null) {
+        // f is a new file
+        console.log(chalk.gray(fileName) + ' created.');
+        run();
+    } else if (curStat.nlink === 0) {
+        // f was removed
+        console.log(chalk.gray(fileName) + ' removed.');
+        run();
+    } else {
+        console.log(chalk.gray(fileName) + ' changed.');
+        run();
+        // f was changed
+    }
+}
 
 function error(err) {
     loading.stop();
