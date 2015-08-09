086e2865ccb797355f94d0a9a9de89f126a7a032
Craig Walker <craig@backbase.com>
2015-08-09 16:19:38 +0200

    bb import yapi for pre master merge

---
+++ commands/import-instances.js
@@ -0,0 +1,101 @@
+var inquirer = require("inquirer");
+var chalk = require('chalk');
+var importCXP = require('../lib/yapi/importCXP');
+var path = require('path');
+var Command = require('ronin').Command;
+
+module.exports = Command.extend({
+    desc: 'Imports portal model from xml files. YAPI CLI.',
+    help: function () {
+        var title = chalk.bold;
+        var d = chalk.gray;
+        var r = '\n  ' + title('Usage') + ': bb ' + this.name + ' [OPTIONS]';
+        r += '\n\t Imports to remote model from XML files. In default mode it takes first XML from the glob and imports it as full portal. In YAPI mode (experimental) merges all XML\'s same as in client side version.';
+        r += '\n\n  ' + title('Options') + ': -short, --name <type> ' + d('default') + ' description';
+        r += '\n\n      -u,  --unattended <boolean>\t' + d('false') + '\t\tSkip all prompts and use options or defaults.';
+        r += '\n      -s,  --search <string>\t\t' + d('*.xml') + '\t\tWild card glob to find Import files.';
+        r += '\n      -v,  --verbose <boolean>\t\t' + d('false') + '\t\tPrints detailed output.';
+
+        r += '\n      -p,  --portal <string> \t\t' + d('<prompt>') + ' \tPre-define portal name.\n';
+        r += '\n      -c,  --context <string>\t\t' + d('portalserver') + '\tThe application context of the portal foundation.';
+        r += '\n      -H,  --host <string>\t\t' + d('localhost') + '\tThe host name of the server running portal foundation.';
+        r += '\n      -P,  --port <number>\t\t' + d('7777') + '\t\tThe port of the server running portal foundation.';
+        r += '\n      -u,  --username <string>\t\t' + d('admin') + '\t\tUsername.';
+        r += '\n      -w,  --password <string>\t\t' + d('admin') + '\t\tPassword.';
+
+        r += '\n\n      -y,  --yapi <boolean>\t\t' + d('false') + '\t\tSwitch to CLI YAPI mode (experimental).';
+        r += '\n      -c,  --sanitizeRemote <boolean>\t\t' + d('true') + '\t\tTo remove redundant remote properties if not matched locally (only in YAPI mode).';
+
+        return r;
+    },
+    options: {
+        sanitizeRemote: {
+            type: 'boolean',
+            alias: 'c',
+            default: true
+        },
+        unattended: {//Get XML directly from this search or CWD without questions
+            type: 'boolean',
+            alias: 'u'
+        },
+        search: {//search using glob wild cards overriding search prompt
+            type: 'string',
+            alias: 's'
+        },
+        cwd: {//change glob to new cwd
+            type: 'string',
+            alias:'w'
+        },
+        verbose: {//console logging to debug level
+            type: 'boolean',
+            alias: 'v'
+        },
+        host: {type: 'string', alias: 'H'},
+        port: {type: 'string', alias: 'P'},
+        context: {type: 'string', alias: 'c'},
+        username: {type: 'string', alias: 'u'},
+        password: {type: 'string', alias: 'w'},
+        portal: {type: 'string', alias: 'p'}
+    },
+    run: function () {
+        var cliOpts = this.options || {};
+
+        if (!cliOpts.search || cliOpts.search.length === 0) {
+            cliOpts.search = path.join(cliOpts.cwd || process.cwd(), '*.xml');
+        } else {
+            cliOpts.search = path.join(cliOpts.cwd || process.cwd(), cliOpts.search);
+        }
+
+        var startImport = (function(){
+            return importCXP.startImport;
+        })();
+
+        if (cliOpts.unattended === true) {
+            startImport(cliOpts);
+        } else {
+
+            var promptCleanup = {
+                message: 'Do you want to remove redundant properties?',
+                name: 'cleanup',
+                type: 'confirm'
+            };
+            var prompts = [];
+
+            if (!cliOpts.sanitizeRemote) {
+                prompts.push(promptCleanup);
+            }
+
+            if (prompts.length > 0) {
+                inquirer.prompt(prompts,
+                    function (answers) {
+                        cliOpts.sanitizeRemote = answers.cleanup || cliOpts.sanitizeRemote;
+
+                        startImport(cliOpts);
+                    }
+                );
+            } else {
+                startImport(cliOpts);
+            }
+        }
+    }
+});
