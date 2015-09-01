f318eed2f2b623b5ec9ef7061bda0244f4a52fea
Guilherme Medeiros <guilherme@backbase.com>
2015-09-01 10:39:39 +0200

    adds generate from archetype command and the project template

---
+++ templates/project/bbscaff.js
@@ -0,0 +1,40 @@
+console.log('This template uses Maven Archetypes, you might need to setup your credentials');
+
+module.exports = function(bbscaff){
+    bbscaff.prompt([
+        {
+            name: 'groupId',
+            message: 'groupId',
+            'default': 'com.mycompany.project'
+        },
+        {
+            name: 'artifactId',
+            message: 'artifactId',
+            'default': function(answers){
+                return answers.groupId.split('.').pop()
+            }
+        },
+        {
+            name: 'version',
+            message: 'version',
+            'default': '1.0-SNAPSHOT'
+        },
+
+        {
+            name: 'launchpad-edition',
+            message: 'launchpad-edition',
+            type: 'list',
+            choices: ['banking', 'universal']
+        }
+
+
+    ], function(answers){
+        bbscaff.archetype(answers, {
+            'archetypeArtifactId': 'launchpad-archetype-CXP5.6',
+            'archetypeGroupId': 'com.backbase.launchpad',
+            'archetypeVersion': '1.0.0-RC'
+        }, function(){
+            console.log('You can now $ cd ' + answers.artifactId);
+        });
+    });
+};
