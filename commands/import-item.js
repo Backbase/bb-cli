983b7f1b9e0868e4584c7f665ff7f4b2f365dd77
Igor Dimitrijevic <igor@backbase.com>
2015-09-07 16:23:23 +0200

    add --verbose option to import-item

--- commands/import-item.js
+++ commands/import-item.js
@@ -28,7 +28,8 @@ module.exports = Command.extend({
         r += '      -w,  --watch <boolean>\t\t' + '\t\tWatch for file changes in the current dir and autosubmit.\n';
         r += '      -a,  --auto <boolean>\t\t' + '\t\tAuto create model.xml if doesn\'t exist.\n';
         r += '      -n,  --name <boolean>\t\t' + '\t\tName of the feature to auto create before reading bower.json\n';
-        r += '      -v,  --version <boolean>\t\t' + '\t\tVersion of the feature to auto create before reading bower.json\n\n';
+        r += '      -v,  --version <boolean>\t\t' + '\t\tVersion of the feature to auto create before reading bower.json\n';
+        r += '           --verbose <boolean>\t\t' + '\t\tEnables detailed output.\n\n';
 
         r += '      -H,  --host <string>\t\t' + d('localhost') + '\tThe host name of the server running portal foundation.\n';
         r += '      -P,  --port <number>\t\t' + d('7777') + '\t\tThe port of the server running portal foundation.\n';
@@ -42,7 +43,10 @@ module.exports = Command.extend({
     options: {
         target: {type: 'string', alias: 't', default: './'},
         watch: {type: 'boolean', alias: 'w'},
-        auto: {type: 'boolean', alias: 'a'}
+        auto: {type: 'boolean', alias: 'a'},
+        name: {type: 'string', alias: 'n'},
+        version: {type: 'string', alias: 'v'},
+        verbose: {type: 'boolean'}
     },
 
     run: function () {
@@ -64,7 +68,7 @@ module.exports = Command.extend({
                         return (v === -1);
                     }
                 }, onWatch);
-                console.log(chalk.cyan('Watching...'));
+                output(chalk.cyan('Watching...'));
             }
 
             return run();
@@ -82,12 +86,12 @@ function run() {
         var replacements = {
             'model.xml': model.getXml()
         };
-        console.log('Creating zip...');
+        output('Creating zip...');
         return zipDir(cfg.target, exclude, replacements)
         .then(function(zip) {
             return bbrest.importItem().file(zip.path).post()
             .then(function(r) {
-                console.log(r);
+                output(r);
                 if (r.error) {
                     throw new Error('Rest API Error: ' + r.statusInfo);
                 }
@@ -104,7 +108,7 @@ function run() {
 }
 
 function prepareModel() {
-    console.log('Reading model.xml...');
+    output('Reading model.xml...');
     return model.read(path.resolve(cfg.target, 'model.xml'))
     .then(function() {
         if (!model.getProperty('version')) {
@@ -142,7 +146,7 @@ function prepareModel() {
 
             return defer.promise
             .then(function() { // --auto options is on
-                console.log('Creating model.xml for feature...');
+                output('Creating model.xml for feature...');
                 if (cfg.name) {
                     model.createFeature(cfg.name);
                     if (cfg.version) {
@@ -169,7 +173,7 @@ function prepareModel() {
 }
 
 function getBowerJson() {
-    console.log('Reading bower.json...');
+    output('Reading bower.json...');
     return fs.readFileAsync(path.resolve(cfg.target, 'bower.json'))
     .then(function(bjson) {
         try {
@@ -187,15 +191,16 @@ function onWatch(fileName, curStat, prevStat) {
     //     // file is object where key is fileName and value is stat
     // } else
     if (prevStat === null) {
+        if (typeof fileName !== 'string') return;
         // f is a new file
-        console.log(chalk.gray(fileName) + ' created.');
+        output(chalk.gray(fileName) + ' created.');
         run();
     } else if (curStat.nlink === 0) {
         // f was removed
-        console.log(chalk.gray(fileName) + ' removed.');
+        output(chalk.gray(fileName) + ' removed.');
         run();
     } else {
-        console.log(chalk.gray(fileName) + ' changed.');
+        output(chalk.gray(fileName) + ' changed.');
         run();
         // f was changed
     }
@@ -203,13 +208,16 @@ function onWatch(fileName, curStat, prevStat) {
 
 function addZeroVersion(model) {
     model.addProperty('version', '0.0.0-alpha.0');
-    console.log('Version can not be resolved. Setting version to ' + model.getProperty('version'));
+    output('Version can not be resolved. Setting version to ' + model.getProperty('version'));
 }
 
+function output() {
+    if (cfg.verbose) console.log.apply(this, arguments);
+}
 function error(err) {
     util.err(chalk.red('bb import-item: ') + (err.message || err.error));
 }
 function ok(r) {
-    util.ok('Importing ' + chalk.yellow(name) + ' from ' + chalk.green(cfg.target) + '. Done.');
+    util.ok(chalk.yellow(name) + ' imported from ' + chalk.green(cfg.target));
     return r;
 }
